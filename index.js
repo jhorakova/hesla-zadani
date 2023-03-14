cf26eadbcca4ff1b8964ae3a4f6e64ca2543b4f3
Jana Horáková <horakovajana2017@gamil.com>
2023-03-14 22:27:02 +0100

    lekce 8 home

--- index.js
+++ index.js
@@ -27,3 +27,34 @@ const strongPassword = (len) => {
 
   return result;
 };
+
+/*Napište funkci createAccount, která se bude tvářit, že zakládá nový uživatelský účet. Funkce bude mít dva parametry user a generatePassword. 
+První bude uživatelské jméno a druhý bude funkce, pomocí které se má vygenerovat heslo pro tento účet. 
+Funkce createAccount vrátí řetězec, který bude obsahovat jméno uživatele a heslo vygenerované voláním funkce generatePassword. 
+Funkci generatePassword při volání předejte číslo 9 jako délku hesla.
+
+Na konci javascriptového kódu vyzkoušejte založit více různých účtů s různými typy hesel. Například:
+
+document.body.innerHTML += `
+  <p>${createAccount('Míša', weakPassword)}</p>
+  <p>${createAccount('Řízek', mediumPassword)}</p>
+  <p>${createAccount('Hustodémon', strongPassword)}</p>
+`
+by mělo vepsat do stránky něco jako:
+
+Uživatel Míša s heslem 012345678
+Uživatel Řízek s heslem 074031827
+Uživatel Hustodémon s heslem mwwf9epts*/
+
+const createAccount = (user, generatePassword) => {
+  return `Uzivatel ${user} s heslem ${generatePassword}`
+}
+
+//console.log(createAccount(user, generatePassword))
+
+document.body.innerHTML = `
+  <p>${createAccount('Míša', weakPassword(9))}</p>
+	<p>${createAccount('Řízek', mediumPassword(9))}</p>
+	<p>${createAccount('Hustodémon', strongPassword(9))}</p>
+`
+
